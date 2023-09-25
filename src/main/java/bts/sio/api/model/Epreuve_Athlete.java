551c0f3ba8a4d7a8b8e1cb35df915aa6ba443a06
adminsio <roussel.louane@gmail.com>
2023-09-25 11:16:25 +0200

    ceation de al class Epreuve_Athlete

---
+++ src/main/java/bts/sio/api/model/Epreuve_Athlete.java
@@ -0,0 +1,28 @@
+package bts.sio.api.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "Epreuve_Athlete")
+public class Epreuve_Athlete {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "Epreuve_id")
+    private Epreuve epreuve_id;
+
+    @ManyToOne
+    @JoinColumn(name = "Athlete_id")
+    private Athlete Athlete_id;
+
+    @Column(name="place")
+    private String place;
+
+
+}
