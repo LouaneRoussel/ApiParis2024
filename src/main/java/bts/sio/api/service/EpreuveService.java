351ce2f93b514573eaf1a73bb4d20f61d5470757
adminsio <roussel.louane@gmail.com>
2023-09-25 11:48:55 +0200

    ceation des services

---
+++ src/main/java/bts/sio/api/service/EpreuveService.java
@@ -0,0 +1,29 @@
+package bts.sio.api.service;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import bts.sio.api.model.Epreuve;
+import java.util.Optional;
+import bts.sio.api.repository.EpreuveRepository;
+@Data
+@Service
+public class EpreuveService {
+    @Autowired
+    private EpreuveRepository EpreuveRepository;
+
+    public Optional<Epreuve> getEpreuve(final Long id) {
+        return EpreuveRepository.findById(id);
+    }
+
+    public Iterable<Epreuve> getLesEpreuve() {
+        return EpreuveRepository.findAll();
+    }
+
+    public void deleteEpreuve(final Long id) {
+        EpreuveRepository.deleteById(id);
+    }
+
+    public Epreuve saveSport(Epreuve epreuve) {
+        return EpreuveRepository.save(epreuve);
+    }
+}
---
+++ src/main/java/bts/sio/api/service/Epreuve_AthleteService.java
@@ -0,0 +1,29 @@
+package bts.sio.api.service;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import bts.sio.api.model.Epreuve_Athlete;
+import java.util.Optional;
+import bts.sio.api.repository.Epreuve_AthleteRepository;
+@Data
+@Service
+public class Epreuve_AthleteService {
+    @Autowired
+    private Epreuve_AthleteRepository Epreuve_AthleteRepository;
+
+    public Optional<Epreuve_Athlete> getEpreuve_Athlete(final Long id) {
+        return Epreuve_AthleteRepository.findById(id);
+    }
+
+    public Iterable<Epreuve_Athlete> getLesEpreuve_Athlete() {
+        return Epreuve_AthleteRepository.findAll();
+    }
+
+    public void deleteEpreuve_Athlete(final Long id) {
+        Epreuve_AthleteRepository.deleteById(id);
+    }
+
+    public Epreuve_Athlete saveSport(Epreuve_Athlete Epreuve_Athlete) {
+        return Epreuve_AthleteRepository.save(Epreuve_Athlete);
+    }
+}
